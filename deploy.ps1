644119f569c2bbb3e2df80d4ca5ce73a15bbcf06
wimmelstein <wim.wiltenbrug@gmail.com>
2020-05-02 19:51:58 +0200

    Prepare application to deploy to tomcat - Powershell script to move artifacts to webapps in tomcat - maven tomcat 7 plugin to deploy - Logging to a file

--- deploy.ps1
+++ deploy.ps1
@@ -1,2 +1,2 @@
-Get-ChildItem D:\tools\apache-tomcat-8.5.54\webapps -Recurse | Remove-Item
-Copy-Item "target\guitarshop-api-0.0.2-SNAPSHOT.war" -Destination "D:\tools\apache-tomcat-8.5.54\webapps\guitar.war"
+Get-ChildItem D:\tools\apache-tomcat-8.5.54\webapps -Recurse  | Remove-Item -Recurse
+Copy-Item "target\guitarshop-api-0.0.2-SNAPSHOT.war" -Destination "D:\tools\apache-tomcat-8.5.54\webapps\guitarshop.war"
