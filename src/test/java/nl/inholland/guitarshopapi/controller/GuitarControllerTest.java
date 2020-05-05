6fdb113e169fbf780891bce58d7b0f8024e6ca0f
wimmelstein <wim.wiltenbrug@gmail.com>
2020-05-05 20:09:00 +0200

    Created BeforeEach method

--- src/test/java/nl/inholland/guitarshopapi/controller/GuitarControllerTest.java
+++ src/test/java/nl/inholland/guitarshopapi/controller/GuitarControllerTest.java
@@ -3,11 +3,13 @@ package nl.inholland.guitarshopapi.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import nl.inholland.guitarshopapi.model.Guitar;
 import nl.inholland.guitarshopapi.service.GuitarService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
@@ -27,9 +29,22 @@ class GuitarControllerTest {
   @Autowired
   private MockMvc mvc;
 
-  @MockBean
-  GuitarService service;
+  @MockBean private GuitarService service;
+  private Guitar guitar;
 
+  @BeforeEach
+  public void setup() {
+    guitar = new Guitar("Fender", "Cougar", 1899);
+  }
+  @Test
+  public void getAllGuitarsShouldReturnJsonArray() throws Exception {
+      given(service.getAllGuitars()).willReturn(Arrays.asList(guitar));
+      this.mvc.perform(get("/guitars")).andExpect(
+              status().isOk())
+              .andExpect(jsonPath("$", hasSize(1)))
+              .andExpect(jsonPath("$[0].brand").value(guitar.getBrand()));
+
+  }
   @Test
   public void callingAllGuitarsShouldReturnOK() throws Exception {
     /*
@@ -40,21 +55,15 @@ class GuitarControllerTest {
   }
 
   @Test
-  public void getAllGuitarsShouldReturnJsonArray() throws Exception {
-      Guitar guitar = new Guitar("Fender", "Cougar", 1899);
-      given(service.getAllGuitars()).willReturn(Arrays.asList(guitar));
-      this.mvc.perform(get("/guitars")).andExpect(
-              status().isOk())
-              .andExpect(jsonPath("$", hasSize(1)))
-              .andExpect(jsonPath("$[0].brand").value(guitar.getBrand()));
-
-  }
-  @Test
   public void postingAGuitarShouldReturn201Created() throws Exception {
     ObjectMapper mapper = new ObjectMapper();
     Guitar guitar = new Guitar("Fender", "Cougar", 1899);
-    this.mvc.perform(post("/guitars").contentType("application/json").content(mapper.writeValueAsString(guitar)))
-            .andExpect(status().isCreated());
+    this.mvc
+        .perform(
+            post("/guitars")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(mapper.writeValueAsString(guitar)))
+        .andExpect(status().isCreated());
   }
 
 }
