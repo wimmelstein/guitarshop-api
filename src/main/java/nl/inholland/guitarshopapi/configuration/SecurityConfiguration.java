6939ad2e29e991c1b67250f0b2ef61e083559081
wimmelstein <wim.wiltenbrug@gmail.com>
2020-05-21 11:28:54 +0200

    Provided custom login page Removed /api endpoint

--- src/main/java/nl/inholland/guitarshopapi/configuration/SecurityConfiguration.java
+++ src/main/java/nl/inholland/guitarshopapi/configuration/SecurityConfiguration.java
@@ -1,11 +1,14 @@
 package nl.inholland.guitarshopapi.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -15,28 +18,43 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
     auth.inMemoryAuthentication()
         .withUser("user")
-        .password("{noop}password") // {noop} means that password is being sent without encoding
+        .password(passwordEncoder().encode("password"))
         .roles("USER")
         .and()
         .withUser("admin")
-        .password("{noop}password")
+        .password(passwordEncoder().encode("password"))
         .roles("ADMIN");
+    System.out.println("Configure done...");
   }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.csrf().disable();
     http.authorizeRequests()
-        .antMatchers("/")
-        .permitAll()
         .antMatchers(HttpMethod.GET, "/guitars/**").permitAll()
         .antMatchers(HttpMethod.POST, "/guitars/**").hasRole("ADMIN")
+        .antMatchers("/**").permitAll()
         .anyRequest().authenticated()
         .and()
-        .formLogin().permitAll();
+        .formLogin()
+        .loginPage("/login.html")
+        .loginProcessingUrl("/perform_login")
+        .defaultSuccessUrl("/create-guitar.html", true)
+        .permitAll()
+        .and()
+        .logout()
+        .logoutUrl("/perform_logout")
+        .deleteCookies("JSESSIONID")
+        .permitAll();
+    System.out.println("Authorize done...");
       }
 
       /*
         In order for this to work, go to https://localhost:8443/api/login
        */
+
+ @Bean
+ public PasswordEncoder passwordEncoder() {
+   return new BCryptPasswordEncoder();
+ }
 }
